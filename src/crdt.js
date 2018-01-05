6e0681ef4553f8dd2d77e8e20b433256a58480e3
Pedro Teixeira <i@pgte.me>
2018-01-05 15:09:48 +0000

    upgraded peer-crdt-ipfs dep and encrypt and decrypt

--- src/crdt.js
+++ src/crdt.js
@@ -3,9 +3,16 @@
 const IPFS = require('ipfs')
 const PeerCRDT = require('peer-crdt')
 const PeerCrdtIpfs = require('peer-crdt-ipfs')
+const Crypto = require('./crypto')
+
+const KEYS = {
+  read: '4XTTM9VV6TEVgtnEcKz5xmFcYHxPXVfYGrAZXdpT4bn1349Yj',
+  write: 'K3TgUXcwYReXvP1Uq9xdsRnNLBx3p7a9VtDk6iaWeWw9fAURdAXxvmdr1mBSJhrgJjmPeBAEMkBd7FDgMug16pmCq8Z14FkcQkDdNoTsjvzmxWgpwbexQtKU6LyhNw8x9au19QXZ'
+}
 
 module.exports = async (type, id) => {
-  const ipfs = new IPFS({
+  const { encrypt, decrypt} = await Crypto(KEYS.read, KEYS.write)
+  const ipfs = window.IPFS = new IPFS({
     EXPERIMENTAL: {
       pubsub: true
     },
@@ -19,7 +26,9 @@ module.exports = async (type, id) => {
   })
 
   const peerCrdtIpfs = PeerCrdtIpfs(ipfs)
-  const CRDT = PeerCRDT.defaults(peerCrdtIpfs)
+  const CRDT = PeerCRDT
+    .defaults(peerCrdtIpfs)
+    .defaults({ encrypt, decrypt })
   const crdt = CRDT.create(type, id)
   await crdt.network.start()
   return crdt
